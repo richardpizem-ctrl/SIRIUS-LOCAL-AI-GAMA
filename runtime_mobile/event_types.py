fe488bdffd9a1f3e943e72ce03d4c8506a2411a6
richardpizem-ctrl <richardpizem@gmail.com>
2026-05-05 18:00:53 +0200

    Add MobileEventTypes class for mobile event types

---
+++ runtime_mobile/event_types.py
@@ -0,0 +1,10 @@
+# SIRIUS LOCAL AI GAMA - Mobile Event Types
+
+class MobileEventTypes:
+    """Centralized list of event types for the mobile runtime."""
+
+    OCR_REQUEST = "ocr_request"
+    TEXT_QUERY = "text_query"
+    SECURITY_CHECK = "security_check"
+    KNOWLEDGE_QUERY = "knowledge_query"
+    UNKNOWN = "unknown"

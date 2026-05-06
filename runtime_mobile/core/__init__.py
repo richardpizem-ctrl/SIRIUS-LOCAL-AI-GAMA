29225caad66036db077ffe2413b9275eae76ed34
richardpizem-ctrl <richardpizem@gmail.com>
2026-05-06 04:14:54 +0200

    Import mobile runtime components in __init__.py

--- runtime_mobile/core/__init__.py
+++ runtime_mobile/core/__init__.py
@@ -1,1 +1,6 @@
+from .runtime_core import MobileRuntimeCore
+from .runtime_context import MobileRuntimeContext
+from .runtime_dispatcher import MobileRuntimeDispatcher
+from .event_types import MobileEvent
+from .router_mobile import MobileNLRouter
 
